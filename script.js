0801cc38a6665d7a71b8eaf46e974a1902eb8197
Dawiz0p <shakespearama@protonmail.com>
2024-10-28 09:22:39 -0400

    index.html

---
+++ script.js
@@ -0,0 +1,20 @@
+/* 
+    1. function name is getComputerChoice
+    2. getComputerChoice randomly selects any of Rock, Paper, Scissors
+    3. Math.random chooses different choices
+*/
+
+const strings = ("rock", "papers", "scissors");
+
+const result = (strings)
+
+const choices = Math.floor(Math.random() * strings.length)
+
+function getComputerChoice(strings) {
+
+
+
+    return (result);
+
+
+}
