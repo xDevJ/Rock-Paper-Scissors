ae26db5b16a81d60feff4265e571046e97f57ed9
Dawiz0p <shakespearama@protonmail.com>
2024-11-04 08:26:19 -0500

    finished playRound function.

--- script.js
+++ script.js
@@ -25,16 +25,16 @@ function getHumanChoice(){
     const userInput = prompt("rock, paper, scissors");
 
     let userInputLower = userInput.toLowerCase();
-    console.log(userInputLower);
+    return userInputLower;
 } 
 
 
 
 /* 
 1. function name playRound !
-2. playRound() has two parameters humanChoice and computerChoice. paramets : humanChoice and computerChoice will take the human and computer choices as arguments.
-3. playRound() will console.log a string "You Lose! Paper beats Rock"
-4. increment humanScore || computerScore based on round winners
+2. playRound() has two parameters humanChoice and computerChoice. paramets : humanChoice and computerChoice will take the human and computer choices as arguments.!
+3. playRound() will console.log a string "You Lose! Paper beats Rock" !
+4. increment humanScore || computerScore based on round winners!
 */
 const humanChoice = getHumanChoice();
 const computerChoice = getComputerChoice();
@@ -45,12 +45,37 @@ function playRound(humanChoice, computerChoice){
         (humanChoice == "paper" && computerChoice == "paper") ||
         (humanChoice == "scissors" && computerChoice == "scissors")
     ){
-        console.log();
+        console.log("Tie");
     } else if(
         (humanChoice == "rock" && computerChoice == "scissors") ||
         (humanChoice == "paper" && computerChoice == "rock") ||
         (humanChoice == "scissors" && computerChoice == "paper")
     ){
-        console.log();
+        console.log(`You Win! ${humanChoice} beats ${computerChoice}`);
+    } else {
+        console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
+    }
+    let humanScore = 0;
+    let computerScore = 0;
+
+    humanScore++;
+
+    computerScore++;
+    if(
+        (humanChoice == "rock" && computerChoice == "rock") ||
+        (humanChoice == "paper" && computerChoice == "paper") ||
+        (humanChoice == "scissors" && computerChoice == "scissors")
+    ){
+        return "Tie";
+    }
+    else if(
+        (humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "scissors" && computerChoice == "paper")
+    ){
+        return humanScore;
+    } else {
+        return computerScore;
     }
 }
+console.log(playRound(humanChoice, computerChoice));
